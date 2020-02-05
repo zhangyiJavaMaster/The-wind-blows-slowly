3fcc7c792a1cccc2fade7b5d680d173eb40551a9
zytest_glb <zytest_pro_glb@163.com>
2020-02-05 18:48:12 +0800

    合入mybatisplus架构代码

---
+++ mybatisplus/src/test/java/com/zy/study/java/mybatisplus/MybatisplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.study.java.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
