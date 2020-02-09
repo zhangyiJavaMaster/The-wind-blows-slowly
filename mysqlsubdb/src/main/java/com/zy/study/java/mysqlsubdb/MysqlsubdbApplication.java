236efbe7a91b9d0ccaa144b3f0afed2f67db5732
zytest_glb <zytest_pro_glb@163.com>
2020-02-09 22:26:24 +0800

    合入动态分库示例项目架构

---
+++ mysqlsubdb/src/main/java/com/zy/study/java/mysqlsubdb/MysqlsubdbApplication.java
@@ -0,0 +1,13 @@
+package com.zy.study.java.mysqlsubdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlsubdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlsubdbApplication.class, args);
+    }
+
+}
---
+++ mysqlsubdb/src/test/java/com/zy/study/java/mysqlsubdb/MysqlsubdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.study.java.mysqlsubdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlsubdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
